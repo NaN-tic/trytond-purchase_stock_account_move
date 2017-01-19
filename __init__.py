2b06832d667732383a0f75112f87edbe81377892
Bernat Brunet Torruella <bernat@nan-tic.com>
2017-01-19 17:02:36 +0100

    prepare code for new versions

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
 from trytond.pool import Pool
-import configuration
-import purchase
+from . import configuration, purchase
 
 
 def register():
