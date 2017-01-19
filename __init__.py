8e58b8bc232e53840a15c812eb905f07cb9db918
Bernat Brunet Torruella <bernat@nan-tic.com>
2017-01-19 17:02:36 +0100

    prepare code for new versions

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
 from trytond.pool import Pool
-from .configuration import *
-from .purchase import *
+from . import configuration, purchase
 
 
 def register():
     Pool.register(
-        Configuration,
-        ConfigurationCompany,
-        Purchase,
-        Move,
-        Line,
+        configuration.Configuration,
+        configuration.ConfigurationCompany,
+        purchase.Move,
+        purchase.MoveLine,
+        purchase.Purchase,
+        purchase.PurchaseLine,
         module='purchase_stock_account_move', type_='model')
