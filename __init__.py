6bfb6c0c2a570704dbde7a45d2f2152516d7cad7
Raimon Esteve <raimon@nan-tic.com>
2018-01-10 14:08:46 +0100

    Import classes from .

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 #The COPYRIGHT file at the top level of this repository contains the full
 #copyright notices and license terms.
 from trytond.pool import Pool
-from . import configuration, purchase, shipment
-
+from . import configuration
+from . import purchase
+from . import shipment
 
 def register():
     Pool.register(
